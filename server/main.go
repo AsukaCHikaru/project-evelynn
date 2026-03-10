5d5805939eea2e59212a13ed4825428874115a2b
asukachikaru <asuka@asukawang.com>
2026-03-10 17:28:56 +0900

    feat: init server

---
+++ server/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+	router.Get("/api/health", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(map[string]string{"status": "ok"})
+	})
+
+	log.Println("server starting on :8080")
+	http.ListenAndServe(":8080", router)
+}
