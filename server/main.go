889f059c24b008ec97a459cd80ed40f1b328a080
asukachikaru <asuka@asukawang.com>
2026-03-16 17:31:47 +0900

    feat: db connection

--- server/main.go
+++ server/main.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/asukachikaru/project-evelynn/server/db"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 func main() {
+	conn, err := sql.Open("pgx", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal("Failed to open db: ", err)
+	}
+	err = conn.Ping()
+	if err != nil {
+		log.Fatal("Failed to connect to db: ", err)
+	}
+	log.Println("Connected to db.")
+	defer conn.Close()
+
+	_ = db.New(conn)
+	
 	router := chi.NewRouter()
 	router.Use(middleware.Logger)
 	router.Get("/api/health", func(w http.ResponseWriter, r *http.Request) {
