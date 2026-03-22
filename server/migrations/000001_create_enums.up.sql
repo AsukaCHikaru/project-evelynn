f4f4afe3d2441d997a43c92910848b2eca2d63b0
asukachikaru <asuka@asukawang.com>
2026-03-22 17:11:08 +0900

    fix: update enums

--- server/migrations/000001_create_enums.up.sql
+++ server/migrations/000001_create_enums.up.sql
@@ -1,4 +1,4 @@
-CREATE TYPE part_of_speech as ENUM ('noun', 'verb', 'adjective', 'adverb', 'preposition');
+CREATE TYPE part_of_speech as ENUM ('noun', 'verb', 'adjective', 'adverb', 'preposition', 'pronoun', 'conjunction', 'article', 'numeral', 'interjection', 'phrase');
 CREATE TYPE difficulty as ENUM ('A1', 'A2', 'B1', 'B2');
 CREATE TYPE gender as ENUM ('masculine', 'feminine', 'neuter');
 CREATE TYPE tense AS ENUM ('present', 'preterite', 'imperfect', 'future', 'subjunctive');
